1fe48f4c15829c955091ec19a3c7e98b2e40c13e
Aawhan <114390114+aawhan0@users.noreply.github.com>
2025-08-14 20:58:55 +0530

    set choices

--- game.py
+++ game.py
@@ -1,7 +1,19 @@
 def play_game():
     print("Welcome to Rock Paper Scissor!")
     variant = input("Choose your game (classis or extended): ").lower().strip()
-    print(f"You selected the {variant} version. Let's Start!")
+
+    #Set choice based on variant:
+    classic_choices = ["rock", "paper", "scissors"]
+    extended_choices = classic_choices + ["lizard", "spock"]
+
+    if variant == "extended":
+        choices = extended_choices
+    else:
+        choices = classic_choices
+
+    print(f"You selected {variant} mode. Choices are: {', '.join(choices)}")
+
+
 
 if __name__ == "__main__":
     play_game()
