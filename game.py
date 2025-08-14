abcee67f1e706989725e91c81648b1e974212151
Aawhan <114390114+aawhan0@users.noreply.github.com>
2025-08-14 23:06:53 +0530

    player input and winner decide

--- game.py
+++ game.py
@@ -1,5 +1,26 @@
 import random
 
+def get_winner(player, computer):
+    wins={
+        "rock": ["scissors"],
+        "paper": ["rock"],
+        "scissors": ["paper"]
+    }
+
+
+    wins_extended= {
+        **wins,
+        "lizard": ["spock", "paper"],
+        "spock": ["scissors", "rock"]
+    }
+
+    if player == computer:
+        return "tie"
+    elif computer in wins_extended.get(player, []):
+        return "player"
+    else:
+        return "computer"
+
 def play_game():
     print("Welcome to Rock Paper Scissor!")
     variant = input("Choose your game (classis or extended): ").lower().strip()
@@ -19,6 +40,17 @@ def play_game():
     computer_choice = random.choice(choices)
     print(f"Computer has made its choice!")
 
+    player_choice = input(f"Choose your move ({', '.join(choices)}):").lower().strip()
+    if player_choice not in choices:
+        print("Invalid choice! Please choose from the list.")
+        return
+
+    print(f"You chose {player_choice}, computer chose {computer_choice}.")
+    winner = get_winner(player_choice, computer_choice)
+    if winner == "tie":
+        print("You win this round!")
+    else:
+        print("Computer wins this round!")    
 
 
 if __name__ == "__main__":
